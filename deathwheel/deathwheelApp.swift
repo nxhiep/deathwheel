c3a086f53f761b4e28a9c24517c4b3e15d095570
Hiep Nguyen <hiepnx27@gmail.com>
2022-03-25 14:31:23 +0700

    Initial Commit

---
+++ deathwheel/deathwheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  deathwheelApp.swift
+//  deathwheel
+//
+//  Created by Hiep Nguyen on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct deathwheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
